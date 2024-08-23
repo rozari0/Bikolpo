ac876e952d45ed5d7a43854b1ff9f5428533af11
rozari0 <61970262+rozari0@users.noreply.github.com>
2024-08-23 21:38:14 +0600

    feature: Add search option

--- products/views.py
+++ products/views.py
@@ -8,5 +8,10 @@ from rest_framework import viewsets, filters
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
+    filter_backends = (
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
     filterset_fields = ["approved", "origin_country"]
+    search_fields = ["name"]
