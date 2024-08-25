5e4b11c121d2159cf8852193f3122ce53f92d5bb
rozari0 <61970262+rozari0@users.noreply.github.com>
2024-08-25 22:09:03 +0600

    fix: remove hyperlink and add id instead.

--- products/serializers.py
+++ products/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from products.models import Product
 
 
-class ProductSerializer(serializers.HyperlinkedModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         exclude = ("approved",)
