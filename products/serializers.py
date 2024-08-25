763eec486390288d3855e527dd7eaa4a114b039e
rozari0 <61970262+rozari0@users.noreply.github.com>
2024-08-25 23:51:00 +0600

    Show Full Name.

--- products/serializers.py
+++ products/serializers.py
@@ -3,8 +3,33 @@ from rest_framework import serializers
 from products.models import Product
 
 
+class RelatedProductSerializer(serializers.ModelSerializer):
+    category = serializers.SerializerMethodField()
+    origin_country = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        exclude = ("approved", "related_product")
+
+    def get_origin_country(self, obj):
+        return obj.get_origin_country_display()
+
+    def get_category(self, obj):
+        return obj.get_category_display()
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.SerializerMethodField()
+    origin_country = serializers.SerializerMethodField()
+    related_product = RelatedProductSerializer(many=True, required=False)
+
     class Meta:
         model = Product
         exclude = ("approved",)
         depth = 1
+
+    def get_origin_country(self, obj):
+        return obj.get_origin_country_display()
+
+    def get_category(self, obj):
+        return obj.get_category_display()
